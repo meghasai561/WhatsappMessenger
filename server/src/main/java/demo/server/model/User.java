fdc546c233858c5d612eec3db74f12c94e809354
meghasai561 <95523591+meghasai561@users.noreply.github.com>
2025-09-07 09:14:07 +0000

    Change User model

--- server/src/main/java/demo/server/model/User.java
+++ server/src/main/java/demo/server/model/User.java
@@ -9,6 +9,7 @@ import lombok.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "users")
 public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
