a2c61f73f34828a1ea834469b8e00651314e923b
meghasai561 <95523591+meghasai561@users.noreply.github.com>
2025-09-07 07:53:30 +0000

    Add global variable in web-client

--- web-client/vite.config.js
+++ web-client/vite.config.js
@@ -12,5 +12,8 @@ export default defineConfig({
       '/media': { target: 'http://localhost:8080', changeOrigin: true },
       '/ws': { target: 'http://localhost:8080', ws: true, changeOrigin: true }
     }
+  },
+  define: {
+    global: 'window'
   }
 })
