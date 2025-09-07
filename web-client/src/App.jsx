3ffecd354e513bce9c60246c7646e5f5b0eddd7a
meghasai561 <95523591+meghasai561@users.noreply.github.com>
2025-09-07 08:08:12 +0000

    Add env file in web-client

--- web-client/src/App.jsx
+++ web-client/src/App.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import SockJS from 'sockjs-client'
 import { Client } from '@stomp/stompjs'
 
-const api = axios.create({})
+const api = axios.create({baseURL: import.meta.env.VITE_API_BASE || 'http://localhost:8080'})
 
 export default function App(){
   const [users, setUsers] = useState([])
