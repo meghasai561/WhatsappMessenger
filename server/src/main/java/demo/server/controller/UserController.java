e13d143c9f43dfa43029f6d849475a23cd540500
meghasai561 <95523591+meghasai561@users.noreply.github.com>
2025-09-07 09:43:23 +0000

    Change URL mapping in userController

--- server/src/main/java/demo/server/controller/UserController.java
+++ server/src/main/java/demo/server/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("api/users")
 @RequiredArgsConstructor
 public class UserController {
     private final ChatService chatService;
